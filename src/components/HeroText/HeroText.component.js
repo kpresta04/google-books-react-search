ec20588ad08761f827d1ecf22f639301372ee30a
Kellen Presta <kpresta04@gmail.com>
2020-05-08 18:43:52 -0500

    form state

---
+++ src/components/HeroText/HeroText.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./heroText.scss";
+
+export default function Types() {
+	return (
+		<div className="heroText">
+			<h1>React Google Books Search</h1>
+			<h3>Find and Save Books of Interest</h3>
+		</div>
+	);
+}
